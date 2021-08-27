817b82580bc3d195fe5506c7bd81a573ecde88aa
jotape00 <jpbasaglia@outlook.com>
2021-08-27 17:48:38 -0300

    Atualização - Consumindo API da model e service

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin> {
+    return this.http.post<UsuarioLogin>("https://blogpessoaldojotape.herokuapp.com/usuarios/logar", usuarioLogin)
+  }
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+    return this.http.post<Usuario>("https://blogpessoaldojotape.herokuapp.com/usuarios/cadastrar", usuario)
+  }
+}
