50d1860b1e971ae286697426a1810a8b2d436807
jotape00 <jpbasaglia@outlook.com>
2021-09-02 16:51:04 -0300

    Implementação do put e delete de temas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { TemaComponent } from './tema/tema.component';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "entrar", pathMatch: "full"},
   {path: "entrar", component: EntrarComponent},
   {path: "cadastrar", component: CadastrarComponent},
   {path: "inicio", component: InicioComponent},
-  {path: "tema", component: TemaComponent}
+  {path: "tema", component: TemaComponent},
+  {path: "tema-edit/:id", component: TemaEditComponent},
+  {path: "tema-delete/:id", component: TemaDeleteComponent}
+
 ];
 
 @NgModule({
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,7 +19,19 @@ export class TemaService {
     return this.http.get<Tema[]>("https://blogpessoaldojotape.herokuapp.com/temas", this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>("https://blogpessoaldojotape.herokuapp.com/temas/${id}", this.token)
+  }
+
   postTema(temaCadastro: Tema): Observable<Tema>{
     return this.http.post<Tema>("https://blogpessoaldojotape.herokuapp.com/temas", temaCadastro, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>("https://blogpessoaldojotape.herokuapp.com/temas", tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete("https://blogpessoaldojotape.herokuapp.com/temas/${id}", this.token)
   }
 }
