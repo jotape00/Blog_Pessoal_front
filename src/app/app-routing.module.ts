05972b2334d08ceb1947b944bb2e848bc907fca3
jotape00 <jpbasaglia@outlook.com>
2021-09-07 18:13:46 -0300

    Implementação do put e delete de postagens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 
 const routes: Routes = [
   {path: "", redirectTo: "entrar", pathMatch: "full"},
@@ -15,7 +17,9 @@ const routes: Routes = [
   {path: "inicio", component: InicioComponent},
   {path: "tema", component: TemaComponent},
   {path: "tema-edit/:id", component: TemaEditComponent},
-  {path: "tema-delete/:id", component: TemaDeleteComponent}
+  {path: "tema-delete/:id", component: TemaDeleteComponent},
+  {path: "postagem-edit/:id", component: PostagemEditComponent},
+  {path: "postagem-delete/:id", component: PostagemDeleteComponent}
 
 ];
 
